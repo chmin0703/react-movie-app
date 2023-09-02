25d79d21e6efc5b5e3138ca392c0d751e7b66ece
chmin0703 <chmz9151@gmail.com>
2023-09-02 10:17:16 +0900

    feat : add header and api folder create

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,18 @@
+import exp from "constants"
+import styles from './Loader.module.css' 
+import loadingBar from '../../assets/loading.svg'
+
+const Loader = () => {
+    return(
+        <div className={styles.Container}>
+            <div className={styles.LoadingContainer}>
+                <img src={loadingBar} alt='loadingBar' />
+                <div className={styles.Title}>
+                    Loading...
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader;
