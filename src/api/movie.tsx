bf81a19a05d4d48f5a909b998096d73f681b4205
chmin0703 <chmz9151@gmail.com>
2023-09-02 11:20:36 +0900

    feat : home api 추가

--- src/api/movie.tsx
+++ src/api/movie.tsx
@@ -8,13 +8,14 @@ const REST_API_ADDR = axios.create({
     baseURL : "https://api.themoviedb.org/3/",
     params : {
         language : "KO-KR",
+        api_key : "f99815a202a8f01d29367f660234cf86"
     },
 })
 
 export const homeApi = {
     nowPlaying : () : Promise<AxiosResponse<NowPlayingData>> => REST_API_ADDR.get("movie/now_playing"),
 
-    movieDetail : (id:number) : Promise<AxiosResponse<AxiosResponse>> => REST_API_ADDR.get(`movie/${id}`,{
+    movieDetail : (id:number) : Promise<AxiosResponse<MovieDetailData>> => REST_API_ADDR.get(`movie/${id}`,{
         params : {append_toresponse : "videos"}
     }),
 }
--- src/screens/HomeScreens/HomeContainer.tsx
+++ src/screens/HomeScreens/HomeContainer.tsx
@@ -1,19 +1,52 @@
 import { Component, ReactNode } from "react";
 import HomePresenter from "./HomePresenter";
+import { homeApi } from "../../api/movie";
 
 interface HomeContainerState {
-    movieDetail : any,
+    nowPlaying : any[] | null;
+    movieDetail : any;
     loading : boolean;
+    error : any;
 }
 
 class HomeContainer extends Component<{}, HomeContainerState> {
     constructor(props : {}){
         super(props);
         this.state = {
+            nowPlaying : null,
             movieDetail : null,
             loading : true,
+            error : null,
         }
     }
+
+    //예약 함수 - 정해져 있는 함수기 때문에 명칭을 바꿀수 없다.
+    async componentDidMount(){
+        try{
+            const {data} = await homeApi.nowPlaying();
+            const movieArray = data.result.map((result:any) => result.id)
+            const movieId = movieArray[Math.floor(Math.random() * movieArray.length)]
+
+            try{
+                const {data : movieDetail} = await homeApi.movieDetail(movieId);
+                if(movieDetail.videos.result.length === 0){
+                    const { data : defaultMovieDetail } = await homeApi.movieDetail(497698);
+                    this.setState({movieDetail : defaultMovieDetail})
+                }else{
+                    this.setState({movieDetail : movieDetail})
+                }
+            }catch(error){
+                this.setState({error : "비디오를 찾을 수 없습니다."})
+            }
+        }
+        catch(error){
+            this.setState({error : "비디오를 재생할 수 없습니다."})
+        }
+        finally {
+            this.setState({loading : false})
+        }
+    }
+    
     render() {
         return <HomePresenter {...this.state} />;
     }
